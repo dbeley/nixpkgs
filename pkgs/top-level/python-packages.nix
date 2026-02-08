52babf422fc7a16c02aee53820466beafd121aae
prophetofxenu <xenu@prophetofxenu.net>
2026-02-08 10:51:29 -0500

    pyedbglib: init at 2.24.2.18

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13428,6 +13428,8 @@ self: super: with self; {
 
   pyecowitt = callPackage ../development/python-modules/pyecowitt { };
 
+  pyedbglib = callPackage ../development/python-modules/pyedbglib { };
+
   pyedflib = callPackage ../development/python-modules/pyedflib { };
 
   pyedimax = callPackage ../development/python-modules/pyedimax { };
